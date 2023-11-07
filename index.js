b860b371da0f1cba9db8be1a5fcf9605a3c71ffb
Oleksandr <admin@admins-MacBook-Pro.local>
2023-11-07 21:59:42 +0200

    edit

--- index.js
+++ index.js
@@ -19,22 +19,34 @@ app.use(express.json());
 
 
 app.post('/auth/register' , registerValidator, async (req, res)=>{
+    try{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json(errors.array());
     }
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hash = await bcrypt.hash(password, salt);
 
 
     const doc = new UserModel({
         email: req.body.email,
-        passwordHash,
+        passwordHash: hash,
     })
 
     const user = await doc.save();
-    res.json(user);
+    const token =jwt.sign({
+        _id: user._id
+    }, 'secret123',{
+        expiresIn: '30d'
+    })
+    const {passwordHash, ...userData} = user._doc
+    res.json({ ...userData, token,});
+} catch(err){
+    res.status(500).json({
+        message: "Не зареєструвався"
+    })
+}
 })
 
 app.listen(4444, (err) => {
